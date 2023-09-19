07ed8db70422eae26d960a33439eac2ce4c32fcd
Shashwath <shash.nayak93@gmail.com>
2023-09-18 23:38:54 -0400

    Working with Events

--- mini-project/dist/index.js
+++ mini-project/dist/index.js
@@ -1,7 +1,17 @@
 "use strict";
 const btn = document.getElementById("btn");
 const input = document.getElementById("todoinput");
-btn.addEventListener("click", function () {
-    alert(input.value);
-    input.value = "";
-});
+const form = document.querySelector("form");
+// form.addEventListener("submit", function(e){
+//     e.preventDefault();
+//     console.log("Submitted");
+// })
+function handleSubmit(e) {
+    e.preventDefault();
+    console.log("Submitted");
+}
+form.addEventListener("submit", handleSubmit);
+// btn.addEventListener("click", function(){
+//     alert(input.value);
+//     input.value="";
+// });
--- mini-project/src/index.ts
+++ mini-project/src/index.ts
@@ -1,9 +1,21 @@
 const btn = document.getElementById("btn")!;
 const input = document.getElementById("todoinput")! as HTMLInputElement;
+const form = document.querySelector("form")!;
 
+// form.addEventListener("submit", function(e){
+//     e.preventDefault();
+//     console.log("Submitted");
+// })
 
-btn.addEventListener("click", function(){
-    alert(input.value);
-    input.value="";
+function handleSubmit(e: SubmitEvent){
+    e.preventDefault();
+    console.log("Submitted");
+}
 
-});
+form.addEventListener("submit",handleSubmit);
+
+// btn.addEventListener("click", function(){
+//     alert(input.value);
+//     input.value="";
+
+// });
