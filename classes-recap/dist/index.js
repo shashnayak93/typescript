f02d105c64836e47f36eb0f7e4521a3c00512f78
Shashwath <shash.nayak93@gmail.com>
2023-09-21 11:58:23 -0400

    Getters and Setters

--- classes-recap/dist/index.js
+++ classes-recap/dist/index.js
@@ -5,12 +5,30 @@ class Player {
         this.first = first;
         this.last = last;
     }
-    getScore(){
+    get fullName(){
+        return `${this.first} ${this.last}`;
+    }
+    set fullName(newName){
+        const [first,last]= newName.split(" ");
+        this.first=first;
+        this.last=last;
+    }
+    get score(){
         return this.#score;
     }
-    updateScore(newScore){
-        this.#score=newScore;
+
+    set score(newScore){
+        if(newScore<0){
+            throw new Error("Score must be positive");
+        }
+        this.#score=newScore
     }
+    // getScore(){
+    //     return this.#score;
+    // }
+    // updateScore(newScore){
+    //     this.#score=newScore;
+    // }
     taunt(){
         console.log("BOOYAH");
     }
@@ -27,9 +45,15 @@ const player1 = new Player("Blue", "Steele");
 // console.log(player1.numLives);
 // player1.lostLife();
 // console.log(player1.numLives);
-console.log(player1.getScore());
-player1.updateScore(28);
-console.log(player1.getScore());
+// console.log(player1.score);
+// player1.score=-57;
+// console.log(player1.score);
+// player1.score=28;
+// console.log(player1.score);
+console.log(player1.fullName);
+player1.fullName="Amy Adams";
+console.log(player1.fullName);
+
 
 
 
