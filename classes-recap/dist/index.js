5d16d2a147e2ae473fdb21648e6b525efdf85930
Shashwath <shash.nayak93@gmail.com>
2023-09-24 18:00:22 -0400

    Super Class

--- classes-recap/dist/index.js
+++ classes-recap/dist/index.js
@@ -49,11 +49,15 @@ class Player {
 }
 
 class AdminPlayer extends Player {
+    constructor(first, last, powers){
+        super(first, last);
+        this.powers=powers;
+    }
     isAdmin = true
 
 }
 
-const admin = new AdminPlayer();
+const admin = new AdminPlayer("admin","mCadmin",["delete", "restore world"]);
 
 
 
