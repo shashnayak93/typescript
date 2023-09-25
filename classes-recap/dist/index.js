6da0802a3488ebbcb2d428f538cad0f1441860e9
Shashwath <shash.nayak93@gmail.com>
2023-09-25 01:10:03 -0400

    Readonly, Public, and Private

--- classes-recap/dist/index.js
+++ classes-recap/dist/index.js
@@ -1,8 +1,14 @@
 "use strict";
 class Player {
+    //public - accessible globally
+    //private - only accessible in the class, can work with the # but you ES2015 for it to work
     constructor(first, last) {
+        this.score = 0;
         this.first = first;
         this.last = last;
+    }
+    secretMethod() {
+        console.log("SECRET METHOD!!!");
     }
 }
 const elton = new Player("Elton", "Steele");
--- classes-recap/src/index.ts
+++ classes-recap/src/index.ts
@@ -1,9 +1,17 @@
 class Player {
-    first: string;
-    last: string;
+    public readonly first: string;
+    //readonly can let other developers know not to modify, and it will red underline in typescript
+    public readonly last: string;
+    private score: number = 0;
+    //public - accessible globally
+    //private - only accessible in the class, can work with the # but you ES2015 for it to work
     constructor(first: string, last: string){
         this.first = first;
         this.last = last;
+    }
+
+    private secretMethod(): void {
+        console.log("SECRET METHOD!!!");
     }
 }
 
