b21756b6086e1a96bcb894db93cea9b8ed053afc
Shashwath <shash.nayak93@gmail.com>
2023-10-16 13:28:06 -0400

    Protected Modifier, Interfaces in TypeScript, and Creating Abstract Classes

--- classes-recap/dist/index.js
+++ classes-recap/dist/index.js
@@ -1,32 +1,59 @@
 "use strict";
-class Player {
-    // public readonly first: string;
-    // //readonly can let other developers know not to modify, and it will red underline in typescript
-    // public readonly last: string;
-    // private score: number = 0;
-    //public - accessible globally
-    //private - only accessible in the class, can work with the # but you ES2015 for it to work
-    constructor(first, last, _score) {
-        this.first = first;
-        this.last = last;
-        this._score = _score;
-    }
-    secretMethod() {
-        console.log("SECRET METHOD!!!");
-    }
-    get fullName() {
-        return `${this.first} ${this.last}`;
-    }
-    get score() {
-        return this._score;
-    }
-    set score(newScore) {
-        if (newScore < 0) {
-            throw new Error("Score cannot be negative!");
-        }
-        this._score = newScore;
+// class Player {
+//     // public readonly first: string;
+//     // //readonly can let other developers know not to modify, and it will red underline in typescript
+//     // public readonly last: string;
+//     // private score: number = 0;
+//     //public - accessible globally
+//     //private - only accessible in the class, can work with the # but you ES2015 for it to work
+//     constructor(public first: string,
+//                 public last: string,
+//                 protected _score: number){
+class Bike {
+    constructor(color) {
+        this.color = color;
     }
 }
-const elton = new Player("Elton", "Steele", 100);
-elton.fullName;
-elton.score = 23;
+class Jacket {
+    constructor(color, brand) {
+        this.color = color;
+        this.brand = brand;
+    }
+    print() {
+        console.log(`${this.color} ${this.brand} jacket`);
+    }
+}
+const bike1 = new Bike("red");
+const jacket1 = new Jacket("black", "Prada");
+class Employee {
+    constructor(first, last) {
+        this.first = first;
+        this.last = last;
+    }
+    greet() {
+        console.log("Hello!");
+    }
+}
+class FullTimeEmployee extends Employee {
+    constructor(first, last, salary) {
+        super(first, last);
+        this.salary = salary;
+    }
+    getPay() {
+        return this.salary;
+    }
+}
+class PartTimeEmployee extends Employee {
+    constructor(first, last, hourlyRate, hoursWorked) {
+        super(first, last);
+        this.hourlyRate = hourlyRate;
+        this.hoursWorked = hoursWorked;
+    }
+    getPay() {
+        return this.hourlyRate * this.hoursWorked;
+    }
+}
+const betty = new FullTimeEmployee("Betty", "White", 95000);
+console.log(betty.getPay());
+const bill = new PartTimeEmployee("Bill", "Billerson", 24, 1100);
+console.log(bill.getPay());
--- classes-recap/src/index.ts
+++ classes-recap/src/index.ts
@@ -1,34 +1,98 @@
-class Player {
-    // public readonly first: string;
-    // //readonly can let other developers know not to modify, and it will red underline in typescript
-    // public readonly last: string;
-    // private score: number = 0;
-    //public - accessible globally
-    //private - only accessible in the class, can work with the # but you ES2015 for it to work
-    constructor(public first: string, public last: string, private _score: number){
+// class Player {
+//     // public readonly first: string;
+//     // //readonly can let other developers know not to modify, and it will red underline in typescript
+//     // public readonly last: string;
+//     // private score: number = 0;
+//     //public - accessible globally
+//     //private - only accessible in the class, can work with the # but you ES2015 for it to work
+//     constructor(public first: string,
+//                 public last: string,
+//                 protected _score: number){
 
-    }
+//     }
 
-    private secretMethod(): void {
-        console.log("SECRET METHOD!!!");
-    }
+//     private secretMethod(): void {
+//         console.log("SECRET METHOD!!!");
+//     }
 
-    get fullName(): string{
-        return `${this.first} ${this.last}`
-    }
+//     get fullName(): string{
+//         return `${this.first} ${this.last}`
+//     }
 
-    get score():number{
-        return this._score;
-    }
+//     get score():number{
+//         return this._score;
+//     }
 
-    set score(newScore: number){
-        if(newScore<0){
-            throw new Error("Score cannot be negative!");
-        }
-        this._score = newScore;
+//     set score(newScore: number){
+//         if(newScore<0){
+//             throw new Error("Score cannot be negative!");
+//         }
+//         this._score = newScore;
+//     }
+// }
+
+// class SuperPlayer extends Player{
+//     public isAdmin: boolean = true;
+//     maxScore(){
+//         this._score = 999999;
+//     }
+// }
+
+// const elton = new Player("Elton", "Steele", 100);
+// elton.fullName;
+// elton.score = 23;
+
+
+interface Colorful{
+    color: string;
+}
+
+interface Printable{
+    print(): void;
+}
+
+class Bike implements Colorful{
+    constructor(public color: string){}
+}
+
+class Jacket implements Colorful, Printable{
+    constructor(public color: string, public brand: string){}
+    print(){
+        console.log(`${this.color} ${this.brand} jacket`);
     }
 }
 
-const elton = new Player("Elton", "Steele", 100);
-elton.fullName;
-elton.score = 23;
+const bike1 = new Bike("red")
+const jacket1 = new Jacket("black", "Prada")
+
+abstract class Employee {
+    constructor(public first: string, public last: string){}
+    abstract getPay():number;
+    greet(){
+        console.log("Hello!");
+    }
+}
+
+class FullTimeEmployee extends Employee{
+    constructor(first: string, last: string, private salary: number){
+        super(first, last);
+    }
+    getPay(): number{
+        return this.salary;
+    }
+}
+
+class PartTimeEmployee extends Employee{
+    constructor(first: string, last: string, private hourlyRate: number, private hoursWorked: number){
+        super(first, last);
+    }
+    getPay(): number{
+        return this.hourlyRate * this.hoursWorked;
+    }
+}
+
+const betty = new FullTimeEmployee("Betty", "White", 95000);
+console.log(betty.getPay());
+
+const bill = new PartTimeEmployee("Bill", "Billerson", 24, 1100);
+console.log(bill.getPay());
