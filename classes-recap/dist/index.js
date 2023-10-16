15953206016d4e26cf2483bbc2ca6a3c6ae26ac8
Shashwath <shash.nayak93@gmail.com>
2023-10-16 10:51:26 -0400

    Parameter Property Shorthand, Getters & Setters

--- classes-recap/dist/index.js
+++ classes-recap/dist/index.js
@@ -1,14 +1,32 @@
 "use strict";
 class Player {
+    // public readonly first: string;
+    // //readonly can let other developers know not to modify, and it will red underline in typescript
+    // public readonly last: string;
+    // private score: number = 0;
     //public - accessible globally
     //private - only accessible in the class, can work with the # but you ES2015 for it to work
-    constructor(first, last) {
-        this.score = 0;
+    constructor(first, last, _score) {
         this.first = first;
         this.last = last;
+        this._score = _score;
     }
     secretMethod() {
         console.log("SECRET METHOD!!!");
     }
+    get fullName() {
+        return `${this.first} ${this.last}`;
+    }
+    get score() {
+        return this._score;
+    }
+    set score(newScore) {
+        if (newScore < 0) {
+            throw new Error("Score cannot be negative!");
+        }
+        this._score = newScore;
+    }
 }
-const elton = new Player("Elton", "Steele");
+const elton = new Player("Elton", "Steele", 100);
+elton.fullName;
+elton.score = 23;
--- classes-recap/src/index.ts
+++ classes-recap/src/index.ts
@@ -1,18 +1,34 @@
 class Player {
-    public readonly first: string;
-    //readonly can let other developers know not to modify, and it will red underline in typescript
-    public readonly last: string;
-    private score: number = 0;
+    // public readonly first: string;
+    // //readonly can let other developers know not to modify, and it will red underline in typescript
+    // public readonly last: string;
+    // private score: number = 0;
     //public - accessible globally
     //private - only accessible in the class, can work with the # but you ES2015 for it to work
-    constructor(first: string, last: string){
-        this.first = first;
-        this.last = last;
+    constructor(public first: string, public last: string, private _score: number){
+
     }
 
     private secretMethod(): void {
         console.log("SECRET METHOD!!!");
     }
+
+    get fullName(): string{
+        return `${this.first} ${this.last}`
+    }
+
+    get score():number{
+        return this._score;
+    }
+
+    set score(newScore: number){
+        if(newScore<0){
+            throw new Error("Score cannot be negative!");
+        }
+        this._score = newScore;
+    }
 }
 
-const elton = new Player("Elton", "Steele");
+const elton = new Player("Elton", "Steele", 100);
+elton.fullName;
+elton.score = 23;
